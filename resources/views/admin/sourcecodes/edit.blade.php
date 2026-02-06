a1924d944798a416a4661367eb9b560988af4794
Wahyu Safrizal <wahyusafrizal174@gmail.com>
2026-02-06 22:52:57 +0700

    update

--- resources/views/admin/sourcecodes/edit.blade.php
+++ resources/views/admin/sourcecodes/edit.blade.php
@@ -20,26 +20,30 @@
 </div>
 @endsection
 
+@push('styles')
+<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-lite.min.css" rel="stylesheet">
+@endpush
+
 @push('scripts')
-<script src="https://cdn.tiny.cloud/1/zjf3oktgtjt6un0nqg7xa4mg1i4u0tikp8xf6dijrguvrv5c/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-lite.min.js"></script>
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-  if (document.getElementById('description')) {
-    tinymce.init({
-      selector: '#description',
-      height: 420,
-      menubar: 'file edit view insert format tools table',
-      plugins: 'lists link image table code charmap preview fullscreen codesample anchor searchreplace wordcount',
-      toolbar: 'undo redo | blocks fontsize | bold italic underline strikethrough | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image table | charmap codesample | removeformat | code fullscreen preview',
-      toolbar_mode: 'wrap',
-      block_formats: 'Paragraf=p; Heading 2=h2; Heading 3=h3; Heading 4=h4; Preformatted=pre',
-      fontsize_formats: '12px 14px 16px 18px 24px 36px',
-      image_advtab: true,
-      image_caption: true,
-      table_toolbar: 'tableprops tabledelete | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol',
-      content_style: 'body { font-family: Inter, system-ui, sans-serif; font-size: 14px; line-height: 1.6; }',
-      promotion: false,
-      branding: false
+$(document).ready(function() {
+  if ($('#description').length) {
+    $('#description').summernote({
+      placeholder: 'Deskripsi lengkap dengan format rich text',
+      height: 320,
+      minHeight: 200,
+      toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture']],
+        ['view', ['codeview', 'fullscreen']]
+      ],
+      styleTags: ['p', 'h2', 'h3', 'h4', 'pre']
     });
   }
 });
